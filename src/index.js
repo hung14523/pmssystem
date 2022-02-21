cd68471f5200ea481cbaa33f8e79d1c64dcd152f
hungbui96 <quanghung445@gmail.com>
2022-02-21 22:29:08 +0900

    Jobsページ作成

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -7,9 +8,14 @@ import reportWebVitals from './reportWebVitals';
 import Jobpage from './scenes/Jobs/jobs';
 
 ReactDOM.render(
+  /*
   <React.StrictMode>
     <Jobpage />
   </React.StrictMode>,
+  */
+  <BrowserRouter>
+    <Jobpage />
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
--- src/scenes/Jobs/jobs.js
+++ src/scenes/Jobs/jobs.js
@@ -1,23 +1,53 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import axios from 'axios';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './jobs.css';
 
 let url = "http://172.16.1.3/api/whcomm/hung/WebLink";
+class ShowList extends React.Component {
+    render() {
+        return (
+            <>
+                <tr>
+                    <td style={{ width: "5%", textAlign: "right" }}><Link to={this.props.ns+"/"+this.props.whcd+"/login"}>{this.props.whcd}</Link></td>
+                    <td style={{ width: "8%" }}><Link to={this.props.ns+"/"+this.props.whcd+"/login"}>{this.props.ns}</Link></td>
+                    <td style={{ width: "20%" }}><Link to={this.props.ns+"/"+this.props.whcd+"/login"}>{this.props.whnm}</Link></td>
+                    <td style={{ width: "8%", textAlign: "center" }}><Link to={this.props.ns+"/"+this.props.whcd+"/login"}>ログイン</Link></td>
+                    <td style={{ width: "8%" }}></td>
+                    <td style={{ width: "8%" }}></td>
+                    <td style={{ width: "8%" }}></td>
+                    <td style={{ width: "5%" }}></td>
+                    <td style={{ width: "30%" }}></td>
+                </tr>
+            </>
+        );
+    }
+}
 class Jobs extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
-            data:[]
+            sokos: [],
+            update_day: '2022/05/02'
         }
     }
     componentDidMount() {
-        axios.get('http://172.16.1.3/api/hung/gpstracking/wh/4',new Headers())
-        .then(res => {
-            console.log(res);
-        });
+        axios.post(url, new URLSearchParams({ 'rtn': 'GetAllSoko^REACTAPI()' }), { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } })
+            .then((response) => {
+                const sokos = JSON.parse(JSON.stringify(response.data['data']));
+                this.setState({ sokos: sokos });
+                //console.log(sokos);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
     }
     render() {
+        const listItems = this.state.sokos.map(soko => (
+            <ShowList key={soko.whcd} whcd={soko.whcd} whnm={soko.whnm} ns={soko.ns} />
+        ));
         return (
             <>
                 <div className="title">
@@ -25,7 +55,7 @@ class Jobs extends React.Component {
                 </div>
                 <div className="updateText">
                     <label>
-                        最終更新日：2020/05/01
+                        最終更新日：{this.state.update_day}
                     </label>
                 </div>
                 <div>
@@ -48,17 +78,7 @@ class Jobs extends React.Component {
                             <tr><th colSpan={9}><a href="http://172.16.1.3/phpinformation.php" target='_blank' rel="noreferrer">PHP設定情報</a></th></tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td style={{ width: "5%", textAlign: "right" }}>4</td>
-                                <td style={{ width: "8%" }}>HUNG</td>
-                                <td style={{ width: "20%" }}>HUNGテスト</td>
-                                <td style={{ width: "8%" }}></td>
-                                <td style={{ width: "8%" }}></td>
-                                <td style={{ width: "8%" }}></td>
-                                <td style={{ width: "8%" }}></td>
-                                <td style={{ width: "5%" }}></td>
-                                <td style={{ width: "30%" }}></td>
-                            </tr>
+                            {listItems}
                         </tbody>
 
                     </table>
